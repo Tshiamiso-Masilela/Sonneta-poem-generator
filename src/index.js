6c80470fac1392c745d39cbeba5fbc491e769c41
Tshiamiso-Masilela <tshiamisomasilela14@gmail.com>
2025-08-07 18:43:05 +0200

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -15,8 +15,12 @@ function generatePoem(event) {
   let apiKey = "18b3c2d737dfoaa59t8ca496b42af0e4";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic poem expert and you love writing short poems in basic HTML, and it should have a maximum of 6 lines. Make sure to follow the users instructions. please don't include the instructions in the poem";
+    "You are a romantic poem expert and you love writing short sonnets in basic HTML, and it should have a maximum of 14 lines. Make sure to follow the users instructions. please don't include the instructions in the poem";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⌛ Generating Poem about ${instructionsInput.value}</div> `;
 
   console.log("generating poem");
   console.log(`prompt ${prompt}`);
